f7c11a89eed7464e1a44befad6f35397f9945f46
szoke3210-cmyk <szoke3210@gmail.com>
2026-07-10 12:37:10 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
 console.log("app.js betöltve");
+const params = new URLSearchParams(window.location.search);
+const kennung = params.get("id");
+const typFilter = params.get("typ");
+const page = params.get("page");
+
+let isAdmin = localStorage.getItem("adminMode") === "true";
+let mitarbeiterMode = localStorage.getItem("mitarbeiterMode") === "true";
+const MITARBEITER_PIN = "2580";
+  
